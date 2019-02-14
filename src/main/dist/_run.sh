eb35a6f2c30b202abd202cc123abd49f907d2f8f
Marek Tutaj <mtutaj@mcw.edu>
2019-02-14 08:15:24 -0600

    updated dependencies

--- src/main/dist/_run.sh
+++ src/main/dist/_run.sh
@@ -5,8 +5,6 @@ APPNAME=SearchIndexer
 APPDIR=/home/rgddata/pipelines/$APPNAME
 
 cd $APPDIR
-DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
-LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
-export SEARCH_INDEXER_OPTS="$DB_OPTS $LOG4J_OPTS"
-
-bin/$APPNAME "$@" 2>&1
+java -Dspring.config=$APPDIR/../properties/default_db.xml \
+    -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+    -jar lib/$APPNAME.jar "$@" 2>&1
